f0a8e42b5645b1576fd82b69ecf66ede3e0c97da
renzo <desarrollo.jadalsoftware@gmail.com>
2024-07-22 14:20:02 -0500

    ErrorHandler updated

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,5 +1,14 @@
 import { Request, Response, NextFunction } from 'express'
-export function errorHandler (error: Error, _req: Request, res: Response, _next: NextFunction): void {
-  console.log('ERRORORORORO :( =', error)
-  res.json({ message: ':(' })
+import { ResponseModel } from '../utils/ReponseModel'
+import { AppError } from '../errors/AppError'
+import { ClientError } from '../errors/ClientError'
+export function errorHandler (error: AppError, _req: Request, res: Response, _next: NextFunction): void {
+  const statusCode = error.statusCode
+  const message = error.message
+  console.error(error)
+  if (error instanceof ClientError) {
+    ResponseModel.error({ res, statusCode, message })
+  } else {
+    ResponseModel.error({ res, message: 'Something went wrong! try again later' })
+  }
 }
