e19f38ef809ceee70cb596c7699bab7231905f6f
renzo <desarrollo.jadalsoftware@gmail.com>
2024-07-22 14:46:41 -0500

    Validation date middlware implemented

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { reportRoute } from './reportRouter'
+import { ValidationDate } from '../middlewares/ValidationDate'
 const router = Router()
 
-export default router.use('/report', reportRoute)
+export default router.use('/report', ValidationDate, reportRoute)
