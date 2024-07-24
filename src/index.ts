065f52d3f29d2872b43a0cd420e452fa57d726a2
renzo <desarrollo.jadalsoftware@gmail.com>
2024-07-24 14:52:14 -0500

    Cors implemented (temporally solution)

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,16 @@
 import express from 'express'
 import routes from './routes'
 import { errorHandler } from './middlewares/errorHandler'
+import cors from 'cors'
 
 // Variables y/o constantes
 const app = express()
 const PORT = process.env.SERVER_PORT ?? 6969
 // Middlewares
 app.use(express.json())
+
+// POR EL MOMENTO
+app.use(cors())
 
 // Rutas
 app.use('/api', routes)
