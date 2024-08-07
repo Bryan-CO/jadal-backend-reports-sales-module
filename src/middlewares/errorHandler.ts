0b97122fc1a39090e71062613b30aa4c42243ded
renzo <desarrollo.jadalsoftware@gmail.com>
2024-08-07 17:34:54 -0500

    feat: logger implemented in errorHandler

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -2,13 +2,15 @@ import { Request, Response, NextFunction } from 'express'
 import { ResponseModel } from '../utils/ReponseModel'
 import { AppError } from '../errors/AppError'
 import { ClientError } from '../errors/ClientError'
+import logger from '../logger/logger'
+
 export function errorHandler (error: AppError, _req: Request, res: Response, _next: NextFunction): void {
   const statusCode = error.statusCode
   const message = error.message
-  console.error(error)
   if (error instanceof ClientError) {
     ResponseModel.error({ res, statusCode, message })
   } else {
+    logger.error(error.message)
     ResponseModel.error({ res, message: 'Something went wrong! try again later' })
   }
 }
