0f7dcde23006f4df45107795d69f8930e7582a71
renzo <desarrollo.jadalsoftware@gmail.com>
2024-08-07 17:34:34 -0500

    feat: testConnection added
    logger was implemented too

--- src/database/dataConnection.ts
+++ src/database/dataConnection.ts
@@ -1,4 +1,5 @@
 import { Pool } from 'pg'
+import logger from '../logger/logger'
 export class DatabaseConnection {
   con: Pool
   constructor () {
@@ -8,5 +9,17 @@ export class DatabaseConnection {
       host: process.env.DB_HOST,
       database: process.env.DB_DATABASE
     })
+  }
+
+  async testConnection (): Promise<void> {
+    try {
+      await this.con.connect()
+      // eslint-disable-next-line
+      logger.info(`${process.env.DB_DATABASE?.toUpperCase()} database connection successful`)
+    } catch (err) {
+      // eslint-disable-next-line
+      logger.error(`${process.env.DB_DATABASE?.toUpperCase()} database connection error. Message: ${err}`)
+      process.exit(1)
+    }
   }
 }
