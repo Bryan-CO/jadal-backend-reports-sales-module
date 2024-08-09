3b7489e56049cb3dec77399811bfecc2d5e6092f
renzo <desarrollo.jadalsoftware@gmail.com>
2024-08-09 14:22:55 -0500

    feat: add SSL condition based on NODE_ENV for production

--- src/database/dataConnection.ts
+++ src/database/dataConnection.ts
@@ -7,7 +7,8 @@ export class DatabaseConnection {
       user: process.env.DB_USERNAME,
       password: process.env.DB_PASSWORD,
       host: process.env.DB_HOST,
-      database: process.env.DB_DATABASE
+      database: process.env.DB_DATABASE,
+      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
     })
   }
 
