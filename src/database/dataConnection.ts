1b2359cdcc9d0b42f1d55f89fb0320593a8c2b75
renzo <desarrollo.jadalsoftware@gmail.com>
2024-08-13 11:33:42 -0500

    feat: connectionTimeoutMillis added and package.json modified for production

--- src/database/dataConnection.ts
+++ src/database/dataConnection.ts
@@ -8,7 +8,8 @@ export class DatabaseConnection {
       password: process.env.DB_PASSWORD,
       host: process.env.DB_HOST,
       database: process.env.DB_DATABASE,
-      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false
+      ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
+      connectionTimeoutMillis: 5000
     })
   }
 
